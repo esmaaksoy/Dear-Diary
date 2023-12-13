9ecf0671eb7daa0d01af8d680fdb05bec0f04a0a
esma <esma.ea.aksoy@gmail.com>
2023-12-13 10:48:31 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import {data} from "./helper/data"
+import { Button } from 'bootstrap';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid="md">
+      <Row>
+        <Col>
+        {data.map((item)=>)}
+        </Col>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/helper/data.js
@@ -0,0 +1,126 @@
+export const data = [
+  {
+    id: 1,
+    months: "January ",
+    day: new Date().toLocaleString(),
+    consulted: true,
+    bg:"#33628F"
+  },
+  {
+    id: 2,
+    months: "February",
+    day: new Date().toLocaleString(),
+    consulted: false,
+  },
+  {
+    id: 3,
+    months: "March",
+    day: new Date().toLocaleString(),
+    consulted: true,
+  },
+  {
+    id: 4,
+    months: "April",
+    day: new Date().toLocaleString(),
+    consulted: true,
+  },
+  {
+    id: 5,
+    months: "May",
+    day: new Date().toLocaleString(),
+    consulted: true,
+  },
+  {
+    id: 6,
+    months: "June",
+    day: new Date().toLocaleString(),
+    consulted: true,
+  },
+  {
+    id: 7,
+    months: "July",
+    day: new Date().toLocaleString(),
+    consulted: true,
+  },
+  {
+    id: 8,
+    months: "August",
+    day: new Date().toLocaleString(),
+    consulted: true,
+  },
+  {
+    id: 9,
+    months: "September",
+    day: new Date().toLocaleString(),
+    consulted: true,
+  },
+  {
+    id: 10,
+    months: "October",
+    day: new Date().toLocaleString(),
+    consulted: true,
+  },
+  {
+    id: 11,
+    months: "November",
+    day: new Date().toLocaleString(),
+    consulted: true,
+  },
+  {
+    id: 12,
+    months: "December",
+    day: new Date().toLocaleString(),
+    consulted: true,
+  },
+];
+
+export const doctorData = [
+  {
+    id: 1,
+    name: "Dr.Hazel Valery",
+    dep: "Cardiology",
+    img: "./img/dr1.jpg",
+  },
+  {
+    id: 2,
+    name: "Dr. Jasmine Carpanter",
+    dep: "Radiolgy",
+    img: "./img/dr2.jpg",
+  },
+  {
+    id: 3,
+    name: "Dr. Arianna Mary",
+    dep: "Orthopedy",
+    img: "./img/dr3.jpg",
+  },
+  {
+    id: 4,
+    name: "Dr. Jessica White",
+    dep: "Gastroentogoly",
+    img: "./img/dr4.jpg",
+  },
+  {
+    id: 5,
+    name: "Harry Coming",
+    dep: "Surgery",
+    img: "./img/dr9.jpg",
+  },
+  {
+    id: 6,
+    name: "Dr. Kristensen Abraham",
+    dep: "Neurology",
+    img: "./img/dr6.jpg",
+  },
+  {
+    id: 7,
+    name: "Dr.Asley Graham",
+    dep: "Pediatry",
+    img: "./img/dr7.jpg",
+  },
+  {
+    id: 8,
+    name: "Dr. Mary Motley",
+    dep: "Oncology",
+    img: "./img/dr8.jpg",
+  },
+];
